0ba3c9b694d023076f390a12ebc9fd9bd81dbf1e
Greg Davis <gdavis111@live.ca>
2018-03-29 23:16:51 +0000

    Added http basic authentication to access admin features

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,9 @@
 class Admin::CategoriesController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
+    ENV['STRIPE_PUBLISHABLE_KEY']
+
   def index
     @categories = Category.order(id: :desc).all
   end
